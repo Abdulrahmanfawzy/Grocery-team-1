d9ff4e139b85efb3934430586cc25e21270aa1bc
Your Name <moaz01140575861@gmail.com>
2026-09-20 18:03:33 +0300

    feat: implement profile dashboard with data fetching and loading state

--- src/features/profile/components/dashboard/RecentOrders.tsx
+++ src/features/profile/components/dashboard/RecentOrders.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui/Button'
 import OrderItem from './OrderItem'
+import type { DashboardData } from '../../types/dashboard.types'
 
-export default function RecentOrders() {
+export default function RecentOrders({ recent_orders }: DashboardData) {
   return (
     <section className="rounded-lg border border-gray-200 bg-white p-4">
       <div className="flex items-center justify-between">
@@ -11,9 +12,16 @@ export default function RecentOrders() {
       </div>
 
       <div className="mt-3">
-        <OrderItem />
-        <OrderItem />
-        <OrderItem last />
+        {recent_orders.map((order) => (
+          <OrderItem
+            delivery_time={order.delivery_time}
+            items_count={order.items_count}
+            status={order.status}
+            total={order.total}
+            id={order.id}
+            key={order.id}
+          />
+        ))}
       </div>
     </section>
   )
--- src/features/profile/pages/DashboardPage.tsx
+++ src/features/profile/pages/DashboardPage.tsx
@@ -4,16 +4,27 @@ import UpcomingDelivery from '../components/dashboard/UpcomingDelivery'
 import ShoppingInsights from '../components/dashboard/ShoppingInsights'
 import TopPurchases from '../components/dashboard/TopPurchases'
 import RecentOrders from '../components/dashboard/RecentOrders'
+import { useDashboard } from '../hooks/userDashboard/useDashboard'
+import Loader from '../components/Loader'
 
 export default function DashboardPage() {
+  const { data, isLoading } = useDashboard()
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <div className="space-y-5">
       {/* Welcome */}
-      <DashboardWelcome />
+      <DashboardWelcome
+        trackOrder={data?.data.track_order ?? 3}
+        loyaltyPoints={data?.data.loyalty_points.points || 10}
+      />
 
       {/* Cart & Delivery */}
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-        <CurrentCart />
+        <CurrentCart last_order={data?.data.last_order} />
         <UpcomingDelivery />
       </div>
 
@@ -22,8 +33,9 @@ export default function DashboardPage() {
 
       {/* Recent Orders & Top Purchases */}
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-        <RecentOrders />
-        <TopPurchases />
+        <RecentOrders recent_orders={data?.data.recent_orders} />
+
+        <TopPurchases top_purchases={data?.data.top_purchases} />
       </div>
     </div>
   )
